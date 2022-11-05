d7737a0845c062b0536126a08a319b055dd33b87
yasirudeshan <it21169144@my.sliit.lk>
2022-11-06 03:32:45 +0530

    intial commit

--- src/main/java/com/customer/AddTicket.java
+++ src/main/java/com/customer/AddTicket.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @WebServlet("/insert")
 
 public class AddTicket extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
